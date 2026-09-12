bc340e465b8974f9f9b38e384e8b71673d9b7b8a
Amar Shah <amarshah74@gmail.com>
2026-09-12 19:59:57 -0400

    Configure WezTerm

--- home.nix
+++ home.nix
@@ -90,5 +90,32 @@ programs.tmux = {
   '';
 };
 
+home.file.".wezterm.lua".text = ''
+  local wezterm = require 'wezterm'
+
+  local config = wezterm.config_builder()
+
+  config.font_size = 14.0
+
+  config.enable_tab_bar = true
+  config.hide_tab_bar_if_only_one_tab = true
+
+  config.window_decorations = "RESIZE"
+
+  config.scrollback_lines = 10000
+
+  config.use_fancy_tab_bar = false
+
+  config.color_scheme = "Builtin Solarized Dark"
+
+  config.window_padding = {
+    left = 10,
+    right = 10,
+    top = 8,
+    bottom = 8,
+  }
+
+  return config
+'';
 
 }
