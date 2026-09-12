816ef1d31afd10be6e4664bd81cd1737b9d308b3
Amar Shah <amarshah74@gmail.com>
2026-09-12 19:41:29 -0400

    Configure zsh with Oh My Zsh and Starship

--- home.nix
+++ home.nix
@@ -3,7 +3,6 @@
 {
   home.username = "amar";
   home.homeDirectory = "/Users/amar";
-
   home.stateVersion = "26.05";
 
   programs.home-manager.enable = true;
@@ -11,5 +10,50 @@
   home.packages = with pkgs; [
     ripgrep
     fd
+    fzf
+    zoxide
+    starship
   ];
+
+  programs.zsh = {
+    enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+
+      plugins = [
+        "git"
+        "sudo"
+        "history"
+        "docker"
+        "aws"
+        "terraform"
+      ];
+
+      theme = "";
+    };
+
+    shellAliases = {
+      ll = "ls -lah";
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 }
