7c5364121fce0b9b92fcc3b3db87afab8ac89e2e
Amar Shah <amarshah74@gmail.com>
2026-09-12 19:57:32 -0400

    Configure tmux

--- home.nix
+++ home.nix
@@ -57,4 +57,38 @@
     enable = true;
     enableZshIntegration = true;
   };
+
+programs.tmux = {
+  enable = true;
+
+  terminal = "screen-256color";
+
+  mouse = true;
+  clock24 = true;
+
+  historyLimit = 100000;
+
+  extraConfig = ''
+    set -g base-index 1
+    setw -g pane-base-index 1
+    set -g renumber-windows on
+
+    set -g status-interval 5
+
+    # Easier pane navigation
+    bind h select-pane -L
+    bind j select-pane -D
+    bind k select-pane -U
+    bind l select-pane -R
+
+    # Easier pane splitting
+    bind | split-window -h
+    bind - split-window -v
+
+    # Reload config
+    bind r source-file ~/.config/tmux/tmux.conf \; display-message "tmux config reloaded"
+  '';
+};
+
+
 }
